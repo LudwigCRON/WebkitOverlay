88b76d478579b5a51ccf8ee21dd8a56d7f5ef3d5
Ludwig CRON <ludwig.cron@gmail.com>
2013-07-08 18:37:20 +0200

    Lundi 15 Juin
    use directshow with aforge.net instead of html5 and win32api

---
+++ WebkitOverlay/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WebkitOverlay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeBtn = new System.Windows.Forms.PictureBox();
+            this.extendBtn = new System.Windows.Forms.PictureBox();
+            this.DeviceList = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.closeBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.extendBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeBtn.Location = new System.Drawing.Point(269, 238);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(44, 44);
+            this.closeBtn.TabIndex = 0;
+            this.closeBtn.TabStop = false;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            this.closeBtn.MouseEnter += new System.EventHandler(this.closeBtn_MouseEnter);
+            this.closeBtn.MouseLeave += new System.EventHandler(this.closeBtn_MouseLeave);
+            // 
+            // extendBtn
+            // 
+            this.extendBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.extendBtn.Location = new System.Drawing.Point(215, 238);
+            this.extendBtn.Name = "extendBtn";
+            this.extendBtn.Size = new System.Drawing.Size(44, 44);
+            this.extendBtn.TabIndex = 1;
+            this.extendBtn.TabStop = false;
+            this.extendBtn.Click += new System.EventHandler(this.extendBtn_Click);
+            this.extendBtn.MouseEnter += new System.EventHandler(this.extendBtn_MouseEnter);
+            this.extendBtn.MouseLeave += new System.EventHandler(this.extendBtn_MouseLeave);
+            // 
+            // DeviceList
+            // 
+            this.DeviceList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.DeviceList.BackColor = System.Drawing.SystemColors.InfoText;
+            this.DeviceList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeviceList.ForeColor = System.Drawing.SystemColors.Info;
+            this.DeviceList.FormattingEnabled = true;
+            this.DeviceList.Location = new System.Drawing.Point(12, 255);
+            this.DeviceList.Name = "DeviceList";
+            this.DeviceList.Size = new System.Drawing.Size(179, 21);
+            this.DeviceList.TabIndex = 2;
+            this.DeviceList.SelectedIndexChanged += new System.EventHandler(this.DeviceList_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(322, 232);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(320, 288);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.DeviceList);
+            this.Controls.Add(this.extendBtn);
+            this.Controls.Add(this.closeBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.closeBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.extendBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox closeBtn;
+        private System.Windows.Forms.PictureBox extendBtn;
+        private System.Windows.Forms.ComboBox DeviceList;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WebkitOverlay/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace WebkitOverlay
+{
+    public partial class Form1 : Form
+    {
+        public FilterInfoCollection videoDevices { get; set; }
+        public VideoCaptureDevice videoSource { get; set; }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //initialize position of the window in order to listen what are writen in config file
+            int x = 0;
+            int y = 0;
+            this.Width = Properties.Settings.Default.width;
+            this.Height = Properties.Settings.Default.height + 48;
+            if (Properties.Settings.Default.top != -1)
+            {
+                y = Properties.Settings.Default.top;
+            }
+            if (Properties.Settings.Default.left != -1)
+            {
+                x = Properties.Settings.Default.left;
+            }
+            if (Properties.Settings.Default.right != -1)
+            {
+                x = Screen.PrimaryScreen.Bounds.Width - Properties.Settings.Default.right - this.Width;
+            }
+            if (Properties.Settings.Default.bottom != -1)
+            {
+                y = Screen.PrimaryScreen.Bounds.Height - Properties.Settings.Default.bottom - this.Height;
+            }
+            this.TopMost = true;
+            this.SetDesktopLocation(x, y);
+
+            //style button
+            if (System.IO.File.Exists("./imgs/close_.png"))
+            {
+                this.closeBtn.Image = Image.FromFile("./imgs/close_.png");
+            }
+            else
+            {
+                MessageBox.Show("Veuillez réinstaller l'application", "Erreur Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+
+            if (System.IO.File.Exists("./imgs/extend_.png"))
+            {
+                this.extendBtn.Image = Image.FromFile("./imgs/extend_.png");
+            }
+            else
+            {
+                MessageBox.Show("Veuillez réinstaller l'application", "Erreur Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+
+            //list video devices
+            ListVideoDevice();
+            ListingInComboBox();
+
+            this.videoSource = null;
+        }
+
+        private void ListVideoDevice()
+        {
+            // enumerate video devices
+            this.videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        }
+
+        private void ListingInComboBox()
+        {
+            this.DeviceList.Items.Clear();
+            foreach (FilterInfo d in this.videoDevices)
+            {
+                this.DeviceList.Items.Add(d.Name + " --- " + d.MonikerString);
+            }
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void closeBtn_MouseEnter(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists("./imgs/close_hover.png"))
+            {
+                this.closeBtn.Image = Image.FromFile("./imgs/close_hover.png");
+            }
+            else
+            {
+                MessageBox.Show("Veuillez réinstaller l'application", "Erreur Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+
+        private void closeBtn_MouseLeave(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists("./imgs/close_.png"))
+            {
+                this.closeBtn.Image = Image.FromFile("./imgs/close_.png");
+            }
+            else
+            {
+                MessageBox.Show("Veuillez réinstaller l'application", "Erreur Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+
+        private void extendBtn_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                if (System.IO.File.Exists("./imgs/extend_.png"))
+                {
+                    this.extendBtn.Image = Image.FromFile("./imgs/extend_.png");
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez réinstaller l'application", "Erreur Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                }
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+                if (System.IO.File.Exists("./imgs/small_.png"))
+                {
+                    this.extendBtn.Image = Image.FromFile("./imgs/small_.png");
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez réinstaller l'application", "Erreur Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                }
+            }
+        }
+
+        private void extendBtn_MouseEnter(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                if (System.IO.File.Exists("./imgs/small_hover.png"))
+                {
+                    this.extendBtn.Image = Image.FromFile("./imgs/small_hover.png");
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez réinstaller l'application", "Erreur Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                }
+            }
+            else
+            {
+                if (System.IO.File.Exists("./imgs/extend_hover.png"))
+                {
+                    this.extendBtn.Image = Image.FromFile("./imgs/extend_hover.png");
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez réinstaller l'application", "Erreur Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                }
+            }
+        }
+
+        private void extendBtn_MouseLeave(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                if (System.IO.File.Exists("./imgs/small_.png"))
+                {
+                    this.extendBtn.Image = Image.FromFile("./imgs/small_.png");
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez réinstaller l'application", "Erreur Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                }
+            }
+            else
+            {
+                if (System.IO.File.Exists("./imgs/extend_.png"))
+                {
+                    this.extendBtn.Image = Image.FromFile("./imgs/extend_.png");
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez réinstaller l'application", "Erreur Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                }
+            }
+        }
+
+        private void DeviceList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!(this.videoSource == null))
+                if (this.videoSource.IsRunning)
+                {
+                    this.videoSource.SignalToStop();
+                    this.videoSource = null;
+                }
+
+            FilterInfo s = this.videoDevices[this.DeviceList.SelectedIndex];
+            // create video source
+            this.videoSource = new VideoCaptureDevice(s.MonikerString);
+            //this.videoSource.DesiredFrameRate = 30;
+            this.videoSource.DesiredFrameSize = new Size(2*this.Width, 2*this.Height);
+            // set NewFrame event handler
+            this.videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            // start the video source
+            this.videoSource.Start();
+            this.pictureBox1.Focus();
+        }
+
+        private void video_NewFrame( object sender, NewFrameEventArgs eventArgs )
+        {
+            // get new frame
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            // process the frame
+            this.pictureBox1.Image = bitmap;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (!(this.videoSource == null))
+                if (this.videoSource.IsRunning)
+                {
+                    this.videoSource.SignalToStop();
+                    this.videoSource = null;
+                }
+        }
+
+    }
+}
